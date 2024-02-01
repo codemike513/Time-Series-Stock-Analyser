ca1e8f63160edb67e7a8211a2cf9b63fa37b3e82
Mihir Pesswani <72348929+codemike513@users.noreply.github.com>
2024-02-01 23:21:18 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import streamlit as st
+
+def reading_data():
+  pass
+
+def rolling():
+  pass
+
+def expanding():
+  pass
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
