aeae75c119a00c7ecb8d7b4cd5cbcaeabd508ed6
Mihir Pesswani <72348929+codemike513@users.noreply.github.com>
2024-02-02 22:31:49 +0530

    Reading Data

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import streamlit as st
+import datetime as dt
+import pandas_datareader as pdr
 
-def reading_data():
-  pass
+def reading_data(stock, sd, ed):
+  data = pdr.DataReader(stock, 'stooq', sd, ed)
+  st.dataframe(data.head())
 
 def rolling():
   pass
@@ -11,7 +14,9 @@ def expanding():
   pass
 
 def main():
-  pass
+  sd = dt.datetime(2000,1,1)
+  ed = dt.datetime(2020,1,1)
+  reading_data('AAPL', sd,ed)
 
 if __name__ == '__main__':
   main()
