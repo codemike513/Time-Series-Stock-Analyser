facb8a0f89e4ee6ca5cdbbb2cbb7483d87735870
Mihir Pesswani <72348929+codemike513@users.noreply.github.com>
2024-02-04 23:54:41 +0530

    Data changes

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ import pandas_datareader as pdr
 
 def reading_data(stock, sd, ed):
   data = pdr.DataReader(stock, 'stooq', sd, ed)
+  st.subheader('Data')
+  st.dataframe(data)
   st.dataframe(data.head())
+  st.dataframe(data.tail())
+  
 
 def rolling():
   pass
@@ -16,7 +20,8 @@ def expanding():
 def main():
   sd = dt.datetime(2000,1,1)
   ed = dt.datetime(2020,1,1)
-  reading_data('AAPL', sd,ed)
+  stock = st.text('Enter Stock Name')
+  reading_data(stock, sd,ed)
 
 if __name__ == '__main__':
   main()
